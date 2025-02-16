71618d28799ed7fd2e07f79cdef03b84126fdb7f
RandomHuman97 <103230029+RandomHuman97@users.noreply.github.com>
2025-02-16 13:37:50 -0500

    hotfix - changed map image

--- src/components/MapChart.jsx
+++ src/components/MapChart.jsx
@@ -26,6 +26,11 @@ export default function MapChart({ inputData }) {
                 .attr('width', '100%')
                 .attr('height', 'auto')
                 .attr('viewBox', `0 0 ${width} ${height}`);
+            svg.append ('image')
+                .attr('xlink:href', backgroundImage)
+                .attr('width', width*2)
+                .attr('height', height*2)
+                .attr('object-fit', 'none');
 
             const processedData = inputData.map((d) => ({
                 x: d.x,
@@ -38,35 +43,38 @@ export default function MapChart({ inputData }) {
                 .y(d => d.y)
                 .weight(d => d.strength)
                 .size([width, height])
-                .bandwidth(40)(processedData);
+                .bandwidth(200)(processedData);
+                
 
             const color = d3.scaleSequential(d3.interpolateViridis)
                 .domain([0, d3.max(densityData, d => d.value)]);
+                
 
             svg.selectAll('path')
                 .data(densityData)
                 .enter().append('path')
                 .attr('d', d3.geoPath())
-                .attr('fill', d => `${color(d.value)}80`);
+                .attr('fill', d => `${color(d.value)}`)
+                .attr('fill-opacity', 0.1);
 
             svg.selectAll('circle')
                 .data(processedData)
                 .enter().append('circle')
                 .attr('cx', d => d.x)
                 .attr('cy', d => d.y)
-                .attr('r', 8)
+                .attr('r', 12)
                 .attr('fill', 'gray')
                 .on('mouseover', function (event, d) {
-                    d3.select(this).attr('r', 10);
+                    d3.select(this).attr('r', 15);
                     svg.append('text')
                         .attr('id', 'tooltip')
-                        .attr('x', d.x + 10)
-                        .attr('y', d.y - 10)
+                        .attr('x', d.x + 15)
+                        .attr('y', d.y - 15)
                         .attr('fill', 'white')
-                        .text(`Humidity: ${d.strength.toFixed(2)}%`);
+                        .text(`${d.strength}% Humidity`);
                 })
                 .on('mouseout', function () {
-                    d3.select(this).attr('r', 8);
+                    d3.select(this).attr('r', 12);
                     svg.select('#tooltip').remove();
                 });
         };
@@ -79,7 +87,6 @@ export default function MapChart({ inputData }) {
             style={{
                 position: 'relative',
                 overflow: 'visible',
-                backgroundImage: `url(${backgroundImage})`,
                 backgroundSize: 'cover'
             }}
         >
